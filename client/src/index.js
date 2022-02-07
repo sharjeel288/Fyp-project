9883627f5d1e7b334654eb08e222024de2f4c5fa
Sharjeel <torasx3600@gmail.com>
2022-02-07 12:34:26 +0500

    initialzie exchange

---
+++ client/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoadingContainer from './Components/LoadingContainer';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <LoadingContainer />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
